2c112356d311b3c885828d05ac040a8813f0bd8a
Santiago Sánchez <sanchez.gsanti@gmail.com>
2024-02-27 15:05:37 -0500

    Tarea de funciones de JS

---
+++ logic.js
@@ -0,0 +1,24 @@
+const Pi = 3.1416;
+const gravedad = 9.8;
+
+let num1 = 4;
+let num2 = 70; 
+let usuario = "Santiago";
+let clave = 1234;
+let persona = true;
+
+function UserInfo(){
+    console.log("Usuario: " + usuario);
+    console.log("Contraseña: " + clave);
+}
+UserInfo();
+
+const areaCirculo = (radio) => {
+    return (radio**2)*Pi;
+};
+console.log("Area: " + areaCirculo(num1));
+
+
+function peso(){
+
+}
