1fd8e834bff65e1d60c36697bf4696ce19959292
Santiago Sánchez <sanchez.gsanti@gmail.com>
2024-02-27 21:28:49 -0500

    Comentarios

--- logic.js
+++ logic.js
@@ -1,24 +1,33 @@
+//Valores Constantes
 const Pi = 3.1416;
 const gravedad = 9.8;
 
+//Variables 
 let num1 = 4;
-let num2 = 70; 
+let num2 = 70.2; 
 let usuario = "Santiago";
 let clave = 1234;
-let persona = true;
+let persona = " kg Adulto";
 
-function UserInfo(){
+//Funciones 
+//Funcion de 0 parametros
+const UserInfo = () => {
     console.log("Usuario: " + usuario);
     console.log("Contraseña: " + clave);
-}
+    console.log("Longitud del usuario "+usuario.length+" caracteres");
+};
 UserInfo();
 
+
+//Funcion y una constante de 1 parametro 
 const areaCirculo = (radio) => {
     return (radio**2)*Pi;
 };
 console.log("Area: " + areaCirculo(num1));
 
 
-function peso(){
-
-}
+//Funcion de mas de 2 parametros con metodo math
+let peso=(aceleracion,masa,tipo)=>{
+    return Math.floor(aceleracion*masa) + tipo
+};
+console.log("Peso: " + peso(gravedad,num2,persona));
